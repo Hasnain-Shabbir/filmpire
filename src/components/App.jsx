6d8fc1bc4f49bb6d0abe45e258078b81025efa18
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-21 02:00:30 +0500

    Setting up the file and folder struture alongside Routing.

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import {
+  NavBar,
+  Actors,
+  Profile,
+  Movies,
+  MovieInformation,
+} from './components';
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path='/' element={<Movies />} />
+          <Route path='/movie/:id' element={<MovieInformation />} />
+          <Route path='actors/:id' element={<Actors />} />
+          <Route path='profile/:id' element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
