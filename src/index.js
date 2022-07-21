02ff80b74bce135bddae12296e2514a45872acbf
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-21 11:13:15 +0500

    Create the Navbar layout.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
   // </React.StrictMode>
 );
